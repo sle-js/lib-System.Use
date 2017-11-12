39e847e8e370f54177de77d9be0fc13b9d6ec518
Graeme Lockley <graemel@no9.co.za>
2017-11-12 16:58:17 +0200

    fix: rather use __dirname than process.cwd()

--- test/TemplateToolTest.js
+++ test/TemplateToolTest.js
@@ -1,22 +1,32 @@
 const Assertion = require("./Libs").Assertion;
 const FileSystem = require("../src/FileSystem");
+const Path = require("path");
 const Unit = require("./Libs").Unit;
 
 const TemplateTool = require("./TemplateTool");
 
+
+const toString = o =>
+    JSON.stringify(o, null, 2);
+
+
+const path = relativeName =>
+    Path.join(Path.dirname(__filename), relativeName);
+
+
 module.exports = Unit.Suite("TemplateToolTest")([
-    TemplateTool.translate(process.cwd() + "/test/unknown.template")
+    TemplateTool.translate(process.cwd() + "/unknown.template")
         .then(_ => false)
         .catch(_ => true)
         .then(result => Unit.Test("Unknown file")(Assertion.isTrue(result))),
 
     Promise.all([
-        TemplateTool.translate(process.cwd() + "/test/sample.template"),
-        FileSystem.readFile(process.cwd() + "/test/expected.sample.js")
+        TemplateTool.translate(path("/sample.template")),
+        FileSystem.readFile(path("/expected.sample.js"))
     ])
         .then(_ => Promise.all([
-            FileSystem.readFile(process.cwd() + "/test/sample.js"),
+            FileSystem.readFile(path("/sample.js")),
             _[1]]))
         .then(_ => Unit.Test("Translate sample.template")(Assertion.equals(_[0])(_[1])))
-        .catch(_ => Unit.Test("Translate sample.template")(Assertion.fail(_)))
+        .catch(_ => Unit.Test("Translate sample.template")(Assertion.fail(toString(_))))
 ]);
